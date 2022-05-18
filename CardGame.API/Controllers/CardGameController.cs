786ef289be7bf7e18b17ef56e970633b0f95ee29
PrateekGangopadhyay <prateekgangopadhyay@gmail.com>
2022-05-18 20:52:38 +0530

    Initial commit

---
+++ CardGame.API/Controllers/CardGameController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CardGame.Business.BC;
+
+namespace CardGame.API.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class CardGameController : ControllerBase
+	{
+		private readonly ILogger<CardGameController> _logger;
+		private CardSimulation _cardGame;
+		public CardGameController(ILogger<CardGameController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpPost(Name = "SimulateCards/V1")]
+		public IActionResult SimulatedCards([FromBody]List<string> items)
+		{
+			if (items == null)
+			{
+				return BadRequest("No cards data found");
+			}
+			else
+			{
+				//List<string> input = new List<string>()
+				//{
+				//	"3C", "JS", "2D", "PT", "10H", "KH", "8S", "4T", "AC", "4H", "RT", "2T"
+				//};
+				_cardGame = new CardSimulation(items);
+				var result = _cardGame.Simulate();
+				return Ok(result);				
+			}
+		}
+	}
+}
---
+++ CardGame.Business/BC/CardSimulation.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame.Business.BC
+{
+	public static class CardSimulationConstants
+	{
+		public static string[] generalCardIndexArray = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+		public static string[] suitsArrayOrdered = { "T", "D", "S", "C", "H" };
+		public static string[] specialCardOrdered = { "4T", "2T", "ST", "PT", "RT" };
+	}
+	public class CardSimulation
+	{
+		private CardCollection _cardCollection;
+		public CardSimulation(List<string> items)
+		{
+			if (items == null) throw new ArgumentNullException("Card numbers are required");
+
+			items.ForEach(item =>
+			{
+				if (string.IsNullOrWhiteSpace(item))
+				{
+					throw new ArgumentNullException("Empty string found");
+				}
+			});
+
+			_cardCollection = new CardCollection(items);
+		}
+
+		public List<string> Simulate()
+		{
+			return _cardCollection.GetSortedCards().Select(x => x.CardValue).ToList();
+		}
+	}
+	internal class Card
+	{
+		public string CardSuite { get; set; }
+		public int CardIndex { get; set; }
+		public bool IsSpecial { get; set; }
+		public string CardValue { get; set; }
+
+		public Card(string str)
+		{
+			CardValue = str;
+			if (str[str.Length - 1] == 'T')
+			{
+				IsSpecial = true;
+				CardIndex = Array.IndexOf(CardSimulationConstants.specialCardOrdered, str) + 1;
+			}
+			else if (str[str.Length - 1] != 'T')
+			{
+				CardIndex = Array.IndexOf(CardSimulationConstants.generalCardIndexArray, str.Substring(0, str.Length - 1)) + 1;
+			}
+			CardSuite = str[str.Length - 1].ToString();
+		}
+	}
+	internal class CardCollection : List<Card>
+	{
+		public CardCollection(List<string> cardStrings)
+		{
+			foreach (var item in cardStrings)
+			{
+				Add(new Card(item));
+			}
+		}
+
+		public List<Card> GetSortedCards()
+		{
+			return this.OrderByDescending(x => x.IsSpecial)
+				.ThenBy(x => x.CardSuite, new CardSuiteComparer())
+				.ThenBy(x => x.CardIndex, new SpecialCardComparer()).ToList();
+		}
+	}
+	internal class SpecialCardComparer : IComparer<int>
+	{
+		public int Compare(int x, int y)
+		{
+			if (x < y) return -1;
+			else if (x > y) return 1;
+			return 0;
+		}
+	}
+	internal class CardSuiteComparer : IComparer<string>
+	{
+		public int Compare(string? x, string? y)
+		{
+			if (Array.IndexOf(CardSimulationConstants.suitsArrayOrdered, x) < Array.IndexOf(CardSimulationConstants.suitsArrayOrdered, y))
+			{
+				return -1;
+			}
+			else if (Array.IndexOf(CardSimulationConstants.suitsArrayOrdered, x) > Array.IndexOf(CardSimulationConstants.suitsArrayOrdered, y))
+			{
+				return 1;
+			}
+			return 0;
+		}
+	}
+}
